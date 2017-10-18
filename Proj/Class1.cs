f56b544c2c63b043dde6e675ec8839dc4d0db592
Nick Muller <muller_nicky@hotmail.com>
2017-10-18 10:42:59 +0200

    Initial commit

---
+++ Proj/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proj
+{
+    public class Class1
+    {
+        private string camelCase; // (1)
+        private string PascalCase;  // (2)
+
+        private string _camelCase;  // (3)
+        private string _PascalCase;  // (4)
+    }
+}
